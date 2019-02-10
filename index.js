9ffdce574909e88e96564ea790887d4f2f457f9d
iamsss <saurav9760@gmail.com>
2019-02-10 15:11:23 +0530

    Opening new Window From Browser Window

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("From Index .js")
+console.log("From Index .js");
+
+const BrowserWindow = require('electron').remote.BrowserWindow;
+const path = require('path');
+const url = require('url');
+const btn = document.getElementById('btnOpen');
+btn.addEventListener('click',function(event){
+
+    let winthree = new BrowserWindow();
+    winthree.loadURL(url.format({
+        pathname: path.join(__dirname, 'one.html'),
+        protocol: 'file',
+        slashes : true
+      }));
+});
